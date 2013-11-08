24b99c8b5c7243af94cc0b1de480578e8f89642f
Sorin <sorin@epigrams.co>
2013-11-08 16:01:50 +0200

    default to env var

--- lib/apple_shove/config.rb
+++ lib/apple_shove/config.rb
@@ -1,6 +1,6 @@
 module AppleShove
   CONFIG = { 
-    :redis_uri     => 'redis://localhost:6379',
+    :redis_uri     => ENV["REDISTOGO_URL"],
     :redis_key        => 'apple_shove',
     :reconnect_timer  => 5              # timeout in minutes to re-establish APNS connection  
   }
